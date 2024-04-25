bbf6820acb450da6651526d30b2091e4a9d68dae
12goal <12gdrv@gmail.com>
2024-04-25 17:41:15 +0800

    Fix loading bundles

--- 12play-freebies-mobile/assets/js/bundle.js
+++ 12play-freebies-mobile/assets/js/bundle.js
@@ -1,12 +1,14 @@
-setTimeout(() => {
-  translator.fetch([LANGUAGES.EN, LANGUAGES.ZH]).then(() => {
-    // -> Translations are ready...
-    translator.translatePageTo(_get_language);
-    changeLanguageColor();
-    initialize();
-    DATE_TIME_LOCALE = _get_language === 'zh' ? 'zh-CN' : 'en-US';
+$(document).ready(function () {
+  setTimeout(() => {
+    translator.fetch([LANGUAGES.EN, LANGUAGES.ZH]).then(() => {
+      // -> Translations are ready...
+      translator.translatePageTo(_get_language);
+      changeLanguageColor();
+      initialize();
+      DATE_TIME_LOCALE = _get_language === 'zh' ? 'zh-CN' : 'en-US';
+    });
   });
-});
+})
 
 /**
  * MENU SLIDE
@@ -118,13 +120,160 @@ function changeLanguageColor() {
 //   });
 // });
 
+setSiteBarMenu = () => {
+  const domain = `${SITE_DOMAIN}/${SITE_COUNTRY.toLowerCase()}/`;
+  const textClass = localStorage["preferred_language"] === "en" ? "" : "lang-cn-bold";
+  const menu = `
+<a href='${domain}fortune-spin.html'" class="">
+  <div class="sidebarFunctionIcon">
+    <img
+      src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/12_slot_menu_icon.png"
+      alt="Malaysia Casino Online Slot Menu Icon"
+      style="width: 58%"
+    />
+  </div>
+  <div class="sidebarFunctionText">
+    <span class="lang-bold ${textClass}" key="12slot">12Lottery</span>
+    <img src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/new.png" alt="" class="menu-flag" style="height: 15px;">
+  </div>
+</a>
+<a href='${domain}goal12.html'" class="">
+  <div class="sidebarFunctionIcon">
+    <img
+      src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/12_goal_menu_icon.png"
+      alt="Malaysia Casino Online Slot Menu Icon"
+      style="width: 58%"
+    />
+  </div>
+  <div class="sidebarFunctionText">
+    <span class="lang-bold ${textClass}" key="12slot">12Goal</span>
+    <img src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/new.png" alt="" class="menu-flag" style="height: 15px;">
+  </div>
+</a>
+<a href='${domain}index.html'">
+  <div class="sidebarFunctionIcon">
+    <img
+      class="ls-is-cached lazyloaded"
+      data-src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/home_menu_icon.png"
+      src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/home_menu_icon.png"
+    />
+  </div>
+  <div class="sidebarFunctionText">
+    <span class="lang-bold ${textClass}" key="sidebar-home">${translator.translateForKey("menu.home")}</span>
+  </div>
+</a>
+<a
+  class="sidebar-Login"
+  href='${domain}myprofile.html'"
+  style="display: none"
+>
+  <div class="sidebarFunctionIcon">
+    <img src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/login_join_menu_icon.png" />
+  </div>
+  <div class="sidebarFunctionText">
+    <span class="lang-bold ${textClass}" key="sidebar-account">${translator.translateForKey("menu.account")}</span>
+  </div>
+</a>
+<a
+  class="sidebar-Login"
+  href='${domain}mydeposit.html'"
+  style="display: none"
+>
+  <div class="sidebarFunctionIcon">
+    <img src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/wallet_menu_button.png" />
+  </div>
+  <div class="sidebarFunctionText">
+    <span class="lang-bold ${textClass}" key="sidebar-wallet">${translator.translateForKey("menu.wallet")}</span>
+  </div>
+</a>
+<a
+  class="sidebar-Login"
+  href='${domain}myinbox.html'"
+  style="display: none"
+>
+  <div class="sidebarFunctionIcon">
+    <img src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/inbox_menu_button.png" />
+  </div>
+  <div class="sidebarFunctionText">
+    <span class="lang-bold ${textClass}" key="sidebar-inbox">${translator.translateForKey("menu.inbox")}</span>
+  </div>
+</a>
 
-
-/**
- * 
- * INITIAL AFTER HAVE translator
- * 
- */
+<a href='${domain}promotion.html'">
+  <div class="sidebarFunctionIcon">
+    <img src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/promo.png" />
+  </div>
+  <div class="sidebarFunctionText">
+    <span class="lang-bold ${textClass}" key="sidebar-promo">${translator.translateForKey("menu.promotion")}</span>
+  </div>
+</a>
+<a href='${domain}vip.html'">
+  <div class="sidebarFunctionIcon">
+    <img src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/vip_menu_icon.png" />
+  </div>
+  <div class="sidebarFunctionText">
+    <span class="lang-bold ${textClass}" key="vip">VIP</span>
+  </div>
+</a>
+<a onclick="window.location = 'https://12playlive.com/${SITE_COUNTRY.toLowerCase()}/'">
+  <div class="sidebarFunctionIcon">
+    <img src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/live-icon.png" />
+  </div>
+  <div class="sidebarFunctionText">
+    <span class="lang-bold ${textClass}" key="e-sport-live"
+      >${translator.translateForKey("menu.liveTv")}</span
+    >
+  </div>
+</a>
+<a href='${domain}download-app.html'">
+  <div class="sidebarFunctionIcon">
+    <img
+      src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/download_menu_icon.png"
+      alt="Malaysia Casino Online Slot Menu Icon"
+      style="width: 65%"
+    />
+  </div>
+  <div class="sidebarFunctionText">
+    <span class="lang-bold ${textClass}" key="sidebar-download"
+      >${translator.translateForKey("menu.downloadApp")}</span
+    >
+  </div>
+</a>
+<div class="regionChg">
+  <div class="sidebarFunctionIcon">
+    <img src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/language_menu_icon.png" />
+  </div>
+  <div class="sidebarFunctionText">
+    <span class="lang-bold ${textClass}" key="sidebar-regionlang"
+      >${translator.translateForKey("menu.language")}</span
+    >
+  </div>
+</div>
+<a href='${domain}contact-us.html'">
+  <div class="sidebarFunctionIcon">
+    <img src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/live_chat_menu_icon.png" />
+  </div>
+  <div class="sidebarFunctionText">
+    <span class="lang-bold ${textClass}" key="sidebar-contactus"
+      >${translator.translateForKey("menu.contactUs")}</span
+    >
+  </div>
+</a>
+<a href='${domain}blog.html'">
+  <div class="sidebarFunctionIcon">
+    <img src="https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/assets/images/menu-icon/info_menu_icon.png" />
+  </div>
+  <div class="sidebarFunctionText">
+    <span class="lang-bold ${textClass}" key="sidebar-blog">${translator.translateForKey("menu.info")}</span>
+  </div>
+</a>
+`;
+  $("#sidebardiv").append(menu);
+  $("#regionOverlay").load(`https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@v0.18/12play-freebies-mobile/region-language.html`);
+  $(".regionChg").click(function () {
+    $("#regionChangeModal").show();
+  });
+};
 
 function initialize() {
 
