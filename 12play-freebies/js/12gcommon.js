f213bd5674192f917a9749bcbdf76857b18f3f2a
12goal <12gdrv@gmail.com>
2025-06-26 14:14:15 +0800

    Fix Term & Conditions label

--- 12play-freebies/js/12gcommon.js
+++ 12play-freebies/js/12gcommon.js
@@ -64,7 +64,7 @@ var translator = new Translator({
   registerGlobally: "__",
   persist: true,
   persistKey: "preferred_language",
-  filesLocation: IS_DEV ? "/12play-freebies-mobile/assets/i18n" : "https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@iframe1.0/12play-freebies-mobile/assets/i18n",
+  filesLocation: IS_DEV ? "/12play-freebies-mobile/assets/i18n" : "https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@iframe1.1/12play-freebies-mobile/assets/i18n",
 });
 
 var PREFERED_REGION = 'preferred_region';
--- 12play-freebies/js/12goal.js
+++ 12play-freebies/js/12goal.js
@@ -99,7 +99,7 @@ getSiteLanguage = async () => {
     registerGlobally: "__",
     persist: true,
     persistKey: "preferred_language",
-    filesLocation: IS_DEV ? "/12play-freebies-mobile/assets/i18n" : "https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@iframe1.0/12play-freebies-mobile/assets/i18n",
+    filesLocation: IS_DEV ? "/12play-freebies-mobile/assets/i18n" : "https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@iframe1.1/12play-freebies-mobile/assets/i18n",
   });
   await translator.fetch([transLang]);
 }
@@ -482,7 +482,7 @@ setupClockCountDown = () => {
 $(document).ready(async function () {
   await getSiteLanguage();
   // const folder = siteLang === 'cn' ? 'chs' : siteLang;
-  // const folderPath = IS_DEV ? '' : 'https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@iframe1.0';
+  // const folderPath = IS_DEV ? '' : 'https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@iframe1.1';
   // $("#header").load(`${folderPath}/12play-freebies/${SITE_COUNTRY.toLowerCase()}/${folder}/header.html`, function () {
   //   $("#4dBtn").addClass("active"); //highlight the nav item
   // });
