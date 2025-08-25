7001e6639aba0d6d40975c6101fb7866c7fce5d0
12goal <12gdrv@gmail.com>
2025-08-25 22:40:18 +0800

    Fix siteLang

--- 12play-freebies/js/12gcommon.js
+++ 12play-freebies/js/12gcommon.js
@@ -42,7 +42,7 @@ var currencyVn = {
   USD: 'USD',
   VND: 'VNĐ',
 }
-
+var siteLang = '';
 const href = location.href;
 if (href.includes('/chs')) {
   siteLang = 'cn';
@@ -65,7 +65,7 @@ var translator = new Translator({
   registerGlobally: "__",
   persist: true,
   persistKey: "preferred_language",
-  filesLocation: IS_DEV ? "/12play-freebies-mobile/assets/i18n" : "https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@iframe1.3/12play-freebies-mobile/assets/i18n",
+  filesLocation: IS_DEV ? "/12play-freebies-mobile/assets/i18n" : "https://cdn.jsdelivr.net/gh/syn-app/12goalcdn@iframe1.4/12play-freebies-mobile/assets/i18n",
 });
 
 var PREFERED_REGION = 'preferred_region';
